d7ede8a47a80c9da20ea9b94f95c1826e32dbffe
drovo-io <info.drovo.io@gmail.com>
2025-11-15 22:59:20 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>LiveGoalX Betting</h1>
+      <p>Coming Soon</p>
+    </div>
+  );
+}
+
+export default App;
